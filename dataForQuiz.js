c0123846c94b0eed96af9f995abb4e3a080e8bde
Adina Totorean <64439268+adina-hub@users.noreply.github.com>
2024-04-04 17:13:45 +0300

    Change changes

---
+++ dataForQuiz.js
@@ -0,0 +1,20 @@
+const citiesData = {
+  Tokyo: {
+    restaurants: ['Sushi Restaurant', 'Ramen Shop', 'Tempura House'],
+    placesToStay: ['Hotel Tokyo', 'Ryokan Shinjuku', 'Airbnb Shibuya'],
+    shoppingPlaces: ['Ginza Shopping District', 'Harajuku Fashion Street', 'Akihabara Electronics Market'],
+    placesToVisit: ['Senso-ji Temple', 'Tokyo Tower', 'Shinjuku Gyoen National Garden']
+  },
+  Kyoto: {
+    restaurants: ['Traditional Kaiseki Restaurant', 'Udon Noodle House', 'Tea House'],
+    placesToStay: ['Ryokan Kyoto', 'Hotel Gion', 'Guesthouse Arashiyama'],
+    shoppingPlaces: ['Nishiki Market', 'Teramachi Street', 'Gion Shopping District'],
+    placesToVisit: ['Kinkaku-ji Temple', 'Fushimi Inari-taisha Shrine', 'Arashiyama Bamboo Grove']
+  },
+  Hiroshima: {
+    restaurants: ['Okonomiyaki House', 'Soba Noodle Shop', 'Izakaya Hiroshima'],
+    placesToStay: ['Hotel Hiroshima', 'Ryokan Miyajima', 'Hostel Peace'],
+    shoppingPlaces: ['Hiroshima Hondori', 'Hiroshima Flea Market', 'Hiroshima Orizuru Tower'],
+    placesToVisit: ['Hiroshima Peace Memorial Park', 'Itsukushima Shrine', 'Shukkeien Garden']
+  }
+};
